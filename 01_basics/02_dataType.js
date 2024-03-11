142e6c4306a1827036ca1b5f6ae93cb88d4d9fe1
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-11 15:22:05 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js newer version
+
+//alert("hello")   //-----we are using nodejs , not browser
+
+/* code readability should be high, ex-
+cosole.log("GOV
+IND");        is the wrong way */
+
+//data type
+
+let name = "GOVIND"
+let age = 18
+let isLOggedIn = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "GOVIND");
+console.log(typeof undefined);
+console.log(typeof null);
