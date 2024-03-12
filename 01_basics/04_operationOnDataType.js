45fde97183685eff03235052104e4fa65267e6b1
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-12 09:20:12 +0000

    operation

---
+++ 01_basics/04_operationOnDataType.js
@@ -0,0 +1,34 @@
+let val = 3
+let negValue = -val
+console.log(negValue);
+
+console.log(2 + 2);  // add
+console.log(2 - 2);  // substract
+console.log(2 * 2);  // multiply
+console.log(2 ** 3); // power
+console.log(2 / 2);  // devide
+console.log(4 % 2);  // remainder
+
+let str1 = "Govind"
+let str2 = " Kumar"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");         // 12
+console.log("1" +2);          // 12
+console.log(1 + 1 + "2");     // 22
+console.log("1" + 1 + 2);     // 122
+console.log("1" + "1" + 2);   // 122
+console.log("1" + "1" + "2"); //122
+
+console.log(true);            // true
+console.log(+true);           // 1
+//console.log(true+);         // error
+console.log("");              // empty
+console.log(+"");             //0
+
+let gameCounter = 100
+console.log(gameCounter);    //100
+console.log(++gameCounter);  //101
+console.log(gameCounter++);  //101
+console.log(gameCounter);    //102
