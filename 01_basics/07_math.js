20cb5dd13a143f74343ab707225adcfb39b4d4a2
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-12 15:29:07 +0000

    math

---
+++ 01_basics/07_math.js
@@ -0,0 +1,26 @@
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.abs(4));
+
+console.log(Math.round(4.65));
+
+console.log(Math.ceil(4.3));
+
+console.log(Math.floor(4.65));
+
+console.log(Math.max(5,6,8,32,123,65));
+
+
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
