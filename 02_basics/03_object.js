fd0d36229a47752c4bcf609a1f28c5c91c6988e5
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-13 14:14:44 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,48 @@
+// singleon
+// object.create
+
+//object literals
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Govind",
+    "full name": "Govind Bhagat",
+    [mySym]: "mykey1",
+    age: 20,
+    location: "Madhubani",
+    email: "haghsja@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Sunday", "Monday"]
+}
+// dot(.) method
+console.log(jsUser.name);
+//console.log(jsUser."full name"); --->not allowed
+
+//square[] method
+console.log(jsUser["name"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+console.log(jsUser["age"]);
+console.log(jsUser["location"]);
+console.log(jsUser["email"]);
+console.log(jsUser["lastLoginDays"]);
+console.log(jsUser["isLoggedIn"]);
+
+
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("hello mr");
+}
+//console.log(jsUser.greeting);
+console.log(jsUser.greeting());
+
+
+jsUser.greeting2 = function(){
+    console.log(`hello mr,${this.name}`);
+}
+//console.log(jsUser.greeting2);
+console.log(jsUser.greeting2());
+
+
+
