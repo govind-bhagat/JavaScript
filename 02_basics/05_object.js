992cd6649d88c4b4d4c9092bc6851601e5ea85fc
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-14 06:07:51 +0000

    object

---
+++ 02_basics/05_object.js
@@ -0,0 +1,27 @@
+//de_structure
+
+const cource = {
+    courceName: "js",
+    price: "999",
+    courceInstructor: "mr "
+}
+
+const {courceInstructor: CI} = cource
+//console.log((courceInstructor));
+console.log(CI);
+
+
+//Api----
+
+//object formate
+// {
+//     "name": "Govind",
+//     "courceName": "JS",
+//     "price": 67
+// }
+
+//array formate   
+// [
+//     {},
+//     {}
+// ]
