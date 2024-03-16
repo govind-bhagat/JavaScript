4e3ee721a6ab2c0005b10a31d9e151fbb88d4494
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-03-16 16:45:57 +0000

    function

---
+++ 03_basics/02_function.js
@@ -0,0 +1,37 @@
+// function calculateCartPrice(...num){
+//   return num
+// }
+//console.log(calculateCartPrice(6,7,8,9,3));
+
+
+// const user = {
+//     username: "Govind",
+//     price: 299
+// }
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+// handleObject(user)
+
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+// handleObject({
+//     username: "madhu",
+//     price: 999
+// })
+
+
+
+// function returnValue(getArray){
+//     return getArray[1]
+// }
+// const myArray = [23,45,67]
+// console.log(returnValue(myArray));
+
+
+function returnValue(getArray){
+    return getArray[1]
+}
+console.log(returnValue([57,54,34]));
