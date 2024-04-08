34f4e7e7d14a6051c94d67d60cc9293c5ca054d5
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2024-04-08 10:38:27 +0000

    defineproperty and descriptor

--- 11_class_and_object/mathPI.js
+++ 11_class_and_object/mathPI.js
@@ -6,29 +6,42 @@
 const descriotor = Object.getOwnPropertyDescriptor(Math, "PI")
 //console.log(descriotor);
 //output ->
- /*                                 {
-                                       value: 3.141592653589793,
+/*                                 {
+                                      value: 3.141592653589793,
 getOwnPropertyDescriptor(Math, "PI")   writable: false,
-                                       enumerable: false,
-                                       configurable: false
-                                    }
+                                      enumerable: false,
+                                      configurable: false
+                                   }
 */
 
 const Tea = {
     name: "jksdah",
     price: 56,
-    isAvailable: true
+    isAvailable: true,
+
+    orderTea: function () {
+        console.log('code fat gaya');
+    }
 }
-//console.log(Tea);
-//console.log(Object.getOwnPropertyDescriptor(Tea, "name"));
+for (let [key, value] of Object.entries(Tea)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} ${value}`);
+    }
+}
+
+
+console.log(Tea);
+console.log(Object.getOwnPropertyDescriptor(Tea, "name"));
 
 Object.defineProperty(Tea, "price", {
     Writable: false,
     enumerable: false
 })
 
-console.log(Object.getOwnPropertyDescriptor(Tea, "price"))
+// console.log(Object.getOwnPropertyDescriptor(Tea, "price"))
 
-for(let [key, value] of Tea){
-    console.log(`${key} ${value}`);
+for (let [key, value] of Object.entries(Tea)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} ${value}`);
+    }
 }
